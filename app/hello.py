9d9307a7c3c06d325eac6af1739d6eea0972ef87
Jenna Finlayson <jennafinlayson@gmail.com>
2018-01-22 00:37:08 -0800

    Add basic web page template and run flask app as part of hello.py. Start button listener on different thread

--- app/hello.py
+++ app/hello.py
@@ -3,9 +3,18 @@
 # pylint: disable=wrong-import-position
 import logging
 import sys
+import threading
 sys.path.append('../')
 from clientlib import fliclib
 from clientlib.button import Button
+from flask import Flask, render_template
+#from wtforms import (Form, StringField, SubmitField, TextAreaField, TextField,
+#                     validators)
+
+# Web app config
+DEBUG = True
+app = Flask(__name__)
+app.config.from_object(__name__)
 
 class FlicLifxApp(object):
     """Main application for flic-lifx
@@ -14,6 +23,7 @@ class FlicLifxApp(object):
     def __init__(self):
         self.client = fliclib.FlicClient("localhost")
         self.buttons = []
+        self.waiter = threading.Event()
 
     def got_button(self, bd_addr):
         """Defines the action to take when we discover a button
@@ -45,6 +55,7 @@ class FlicLifxApp(object):
         """
         for bd_addr in items["bd_addr_of_verified_buttons"]:
             self.got_button(bd_addr)
+        self.waiter.set() # We now have button info
 
     def hello(self):
         """Gets button info from flic client, sets action to take
@@ -52,7 +63,23 @@ class FlicLifxApp(object):
         """
         self.client.get_info(self.got_info)
         self.client.on_new_verified_button = self.got_button
-        self.client.handle_events()
+
+        # Start main button listener thread
+        threading.Thread(target=self.client.handle_events).start()
+
+        # Waits for got_info() to be called, then logs the info
+        self.waiter.wait()
+        for i in self.buttons:
+            logging.info(i)
+
+        # Start main HTTP app server
+        app.run(host='0.0.0.0', use_reloader=False)
+
+@app.route("/", methods=['GET', 'POST'])
+def start():
+    """Starts the main http web app
+    """
+    return render_template('main.html')
 
 if __name__ == "__main__":
     logging.basicConfig(filename='flic-lifx.log', filemode='w', level=logging.DEBUG)
